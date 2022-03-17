3d360aa8ec5db2bd78e6813a72b1fd7ad4d48054
Isaak Morales <ice.isaak97@gmail.com>
2022-03-17 14:54:25 -0400

    Rewrote the function after user input if it doesn't meet the criteria

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ var numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
 var lettersUpper = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 var lettersLower = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 var symbols = ["`", "~", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "_", "+", "=", "-", "[", "]", "{", "}", "|", "/", "?", ".", ">", ",", "<", ";"];
-    
+var all = []
 
 // Write password to the #password input
 function writePassword() {
@@ -24,18 +24,21 @@ function generatePassword() {
 var userPass = parseInt(prompt("Choose a length of at least 8 characters and no more than 128 characters"))
 console.log(userPass);
 
-if (isNaN(userPass)) {
-alert ("Must be a number")
-} 
+//* can use return or rewrite the function after the alert
 
-//TODO Ask difference between prompt and alert
+if (isNaN(userPass)) {
+alert ("Must be a number") 
+generatePassword()
+}
+
 if (userPass < 8) {
-    alert ("Must be 8 characters long")
-} 
-console.log("Must be less than 128 characters");
+alert ("Must be at least 8 characters long")
+generatePassword()
+}
 
 if (userPass > 128) {
-    alert ("Must be less than 128 characters")
+alert ("Must be less than 128 characters")
+generatePassword()    
 }
 
 //*New variables made to go through new prompts
@@ -55,6 +58,7 @@ var hasSymbols = confirm("Press ok if you want to have symbols in your password.
 //*Must select one of the criteria if selected false for all of the prompts
 if (hasNum === false && hasUpper === false && hasLower === false && hasSymbols === false) {
     alert ("Must meet one of the criteria")
+    return;
     } 
 
 }
