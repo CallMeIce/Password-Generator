9475d612869846adb66886f50b85a36c0cdd88c3
Isaak Morales <ice.isaak97@gmail.com>
2022-03-16 20:51:34 -0400

    Variables are set and still working on the alert medias up

--- script.js
+++ script.js
@@ -1,54 +1,56 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+var numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+var lettersUpper = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+var lettersLower = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var symbols = ["`", "~", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "_", "+", "=", "-", "[", "]", "{", "}", "|", "/", "?", ".", ">", ",", "<", ";"];
+    
+
 // Write password to the #password input
-//TODO function writePassword() {
-//TODO  var password = generatePassword();
-//TODO  var passwordText = document.querySelector("#password");
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
 
-//TODO  passwordText.value = password;
+  passwordText.value = password;
 
+}
 
 // Add event listener to generate button
-//TODO generateBtn.addEventListener("click", writePassword);
+generateBtn.addEventListener("click", writePassword);
 
+function generatePassword() {
+    
+var userPass = parseInt(prompt("Choose a length of at least 8 characters and no more than 128 characters"))
+console.log(userPass);
 
-//TODO var number(window.prompt)("How many characters would you like your password? REMEMBER 8 through 128")
+if (isNaN(userPass)) {
+alert ("Must be a number")
+}
 
+if (userPass < 8) {
+    alert ("Must be 8 characters long")
+} 
+console.log("Must be less than 128 characters");
 
+if (userPass > 128) {
+    alert ("Must be less than 128 characters")
+}
 
-// GIVEN I need a new, secure password
-// WHEN I click the button to generate a password
+var hasNum = confirm("Press ok if you want numbers in your password.")
+    console.log(hasNum);
 
+var hasUpper = confirm("Press ok if you want uppercase letters in your password.")
+    console.log(hasUpper)
 
-// THEN I am presented with a series of prompts for password criteria
-// WHEN prompted for password criteria
+var hasLower = confirm("Press ok if you want lowercase letters in your password.")
+    console.log(hasLower);
 
+var hasSymbols = confirm("Press ok if yo want to have symbols in your password.")
+    console.log(hasSymbols);
 
-// THEN I select which criteria to include in the password
-// WHEN prompted for the length of the password
+if (hasNum === false && hasUpper === false && hasLower === false && hasSymbols === false) {
+    alert ("Must meet one of the criteria")    
+} 
 
-
-// THEN I choose a length of at least 8 characters and no more than 128 characters
-// WHEN asked for character types to include in the password
-
-
-// THEN I confirm whether or not to include lowercase, uppercase, numeric, and/or special characters
-// WHEN I answer each prompt
-
-
-// THEN my input should be validated and at least one character type should be selected
-// WHEN all prompts are answered
-
-
-// THEN a password is generated that matches the selected criteria
-// WHEN the password is generated
-
-
-// THEN the password is either displayed in an alert or written to the page
-
-
-//* 0123456789
-//* abcdefghijklmnopqrstuvwxyz
-//* ABCDEFGHIJKLMNOPQRSTUVWXYZ
-//* ~`!@#$%^&*()_+=-[]{}\|/?.>,<
+}
